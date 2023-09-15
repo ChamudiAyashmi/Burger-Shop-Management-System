237ef28134444988797d1a8d4c8920b99ab144e5
ChamudiAyashmi <chamudi2003@gmail.com>
2023-09-15 23:00:43 +0530

    update

---
+++ Back-End/src/main/java/edu/example/controller/CustomerController.java
@@ -0,0 +1,15 @@
+package edu.example.controller;
+
+import edu.example.dto.Customer;
+import edu.example.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+@CrossOrigin
+public class CustomerController {
+
+
+
+}
---
+++ Back-End/src/main/java/edu/example/dto/Order.java
@@ -0,0 +1,9 @@
+package edu.example.dto;
+
+public class Order {
+    private String orderId;
+    private String description;
+    private int qty;
+    private double unitPrice;
+
+}
